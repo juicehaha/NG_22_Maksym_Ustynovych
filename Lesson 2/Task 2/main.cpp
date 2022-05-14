44fd71c5707de31badc56b87a1897a46b182dbbd
juicehaha <id.maksim0000@gmail.com>
2022-05-14 15:12:31 +0300

    implemented task without changes of users input

--- Lesson 2/Task 2/main.cpp
+++ Lesson 2/Task 2/main.cpp
@@ -5,19 +5,23 @@ using namespace std;
 int main()
 {
     int number = 0;
+    int number1 = 0;
     int sum = 0;
     int temp = 0;
 
     cout << "Enter your number: ";
     cin >> number;
 
-    while (number != 0)
+    number1 = number;
+
+    while (number1 != 0)
     {
-        temp = number %10;
-        if (temp %2 == 0){
+        temp = number1 %10;
+        if (temp %2 == 0)
+        {
             sum += temp;
         }
-        number = number/10;
+        number1 = number1/10;
     }
     cout << "Sum of even numbers are: " << sum;
 
