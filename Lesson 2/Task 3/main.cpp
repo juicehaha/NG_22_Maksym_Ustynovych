50f1191e917c0a0a9e3dc086120cc6d3f7762f16
juicehaha <id.maksim0000@gmail.com>
2022-05-14 15:12:46 +0300

    implemented task without changes of users input

--- Lesson 2/Task 3/main.cpp
+++ Lesson 2/Task 3/main.cpp
@@ -5,20 +5,25 @@ using namespace std;
 int main()
 {
     int number = 0;
+    int number1 = 0;
     int hashcode = 0;
 
     cout << "Enter your number: ";
     cin >> number;
 
- while (number!=0){
-        if (number % 10 == number /10 % 10 ){
-            hashcode += number % 10;
+    number1 = number;
+
+    while (number1!=0){
+        if (number1 % 10 == number1 /10 % 10 )
+        {
+            hashcode += number1 % 10;
         }
 
-        if (number < 10 && number == number % 10){
-			hashcode += number;
+        if (number1 < 10 && number1 == number1 % 10)
+        {
+			hashcode += number1;
         }
-            number /= 10;
+        number1 /= 10;
     }
     cout << "Hashcode = " << hashcode;
 
